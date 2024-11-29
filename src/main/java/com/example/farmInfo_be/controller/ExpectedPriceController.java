4a379514cbaca99b154f90d00b296e24bfcf50ed
권용현 <gwon-yonghyeon@gwon-yonghyeon-ui-MacBookAir.local>
2024-11-30 01:23:12 +0900

    [Chore] cors 해결

--- src/main/java/com/example/farmInfo_be/controller/ExpectedPriceController.java
+++ src/main/java/com/example/farmInfo_be/controller/ExpectedPriceController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/price")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ExpectedPriceController {
     private final ExpectedPriceService priceService;
 
--- src/main/java/com/example/farmInfo_be/controller/ShipmentAmountController.java
+++ src/main/java/com/example/farmInfo_be/controller/ShipmentAmountController.java
@@ -7,14 +7,12 @@ import com.example.farmInfo_be.repository.MemberRepository;
 import com.example.farmInfo_be.repository.ShipmentReportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/shipments")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ShipmentAmountController {
     private final ShipmentReportRepository shipmentReportRepository;
     private final MemberRepository memberRepository;
