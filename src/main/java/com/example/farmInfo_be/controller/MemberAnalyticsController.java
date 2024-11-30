8c6937b0a87d59d3a32a51a8d54ce6650bd21d1e
권용현 <gwon-yonghyeon@gwon-yonghyeon-ui-MacBookAir.local>
2024-11-30 15:35:03 +0900

    [Chore] controller 추가

--- src/main/java/com/example/farmInfo_be/controller/MemberAnalyticsController.java
+++ src/main/java/com/example/farmInfo_be/controller/MemberAnalyticsController.java
@@ -17,24 +17,24 @@ import org.springframework.web.bind.annotation.*;
 public class MemberAnalyticsController {
     private final MemberAnalyticsService analyticsService;
 
-//    @Operation(summary = "회원 재배/출하 분석 (현재 년도)", description = "회원의 현재 년도 품목별 비율과 면적당 출하량을 분석합니다.")
-//    @GetMapping
-//    public ResponseEntity<MemberAnalyticsResponse> getAnalytics(
-//            @Parameter(description = "회원 전화번호", required = true) @RequestParam String phoneNumber
-//    ) {
-//        System.out.println("MemberAnalyticsController.getAnalytics");
-//        System.out.println("phoneNumber = " + phoneNumber);
-//        return ResponseEntity.ok(analyticsService.getAnalytics(phoneNumber));
-//    }
-//
-//    @Operation(summary = "회원 재배/출하 분석 (전년도)", description = "회원의 전년도 품목별 비율과 면적당 출하량을 분석합니다.")
-//    @GetMapping("/prev-year")
-//    public ResponseEntity<MemberAnalyticsResponse> getAnalyticsPrevYear(
-//            @Parameter(description = "회원 전화번호", required = true) @RequestParam String phoneNumber
-//    ) {
-//        System.out.println("MemberAnalyticsController.getAnalyticsPrevYear");
-//        System.out.println("phoneNumber = " + phoneNumber);
-//        return ResponseEntity.ok(analyticsService.getAnalyticsPrevYear(phoneNumber));
-//    }
+    @Operation(summary = "회원 재배/출하 분석 (현재 년도)", description = "회원의 현재 년도 품목별 비율과 면적당 출하량을 분석합니다.")
+    @GetMapping
+    public ResponseEntity<MemberAnalyticsResponse> getAnalytics(
+            @Parameter(description = "회원 전화번호", required = true) @RequestParam String phoneNumber
+    ) {
+        System.out.println("MemberAnalyticsController.getAnalytics");
+        System.out.println("phoneNumber = " + phoneNumber);
+        return ResponseEntity.ok(analyticsService.getAnalytics(phoneNumber));
+    }
+
+    @Operation(summary = "회원 재배/출하 분석 (전년도)", description = "회원의 전년도 품목별 비율과 면적당 출하량을 분석합니다.")
+    @GetMapping("/prev-year")
+    public ResponseEntity<MemberAnalyticsResponse> getAnalyticsPrevYear(
+            @Parameter(description = "회원 전화번호", required = true) @RequestParam String phoneNumber
+    ) {
+        System.out.println("MemberAnalyticsController.getAnalyticsPrevYear");
+        System.out.println("phoneNumber = " + phoneNumber);
+        return ResponseEntity.ok(analyticsService.getAnalyticsPrevYear(phoneNumber));
+    }
 }
 
